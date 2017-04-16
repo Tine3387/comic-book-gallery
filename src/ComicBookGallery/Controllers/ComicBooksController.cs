ff4d912d14c6877f0f52770e1d2e2ce3aa8e56ab
Tine Rasmussen <tine.rasmussen7@gmail.com>
2017-04-16 15:52:04 +0200

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,27 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            /* ViewBag is an object provided by MVC, that allows us to pass data from our controller to a view. */
+            /* Capatilize the letter after the ViewBag. since we switching from using variables to properties, */
+            /* we need to capatilize the first letter of each property name. */
+            /* We can see that it is of type dynamic. */
+            /* This allows us to define properties on the ViewBag object, */
+            /* without having to modify a class as we normally have to do. */
+            /* ViewBag is one of the few real world use cases for dynamic types in C#. */
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last great act of revenge! Even if Spider Man survives...<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                /* To comment out multiple lines, select the lines, hold down the ctrl + k following by c */
+                /* To uncomment, select the lines, hold down ctrl + k following by u */
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
